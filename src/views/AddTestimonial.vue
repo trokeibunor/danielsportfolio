9ffe51efd7cf90188e80b341fbf308171c044795
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2022-12-07 22:45:02 +0100

    pages update"

---
+++ src/views/AddTestimonial.vue
@@ -0,0 +1,72 @@
+<template>
+  <NavbarComponentVue />
+  <div class="passwordBlock"></div>
+  <div class="testContainer">
+    <h3>Add Testimonial</h3>
+    <form action="#">
+      <input type="text" name="name" id="" placeholder="Name*" required />
+      <input type="email" name="" id="" placeholder="Email*" required />
+      <textarea name="testimonial" placeholder="Testimonial"></textarea>
+      <button type="submit">
+        Submit <img src="../assets/submit_icon.svg" alt="" />
+      </button>
+    </form>
+  </div>
+  <FooterComponentVue />
+</template>
+
+<script setup>
+import NavbarComponentVue from "../components/NavbarComponent.vue";
+import FooterComponentVue from "../components/FooterComponent.vue";
+</script>
+
+<style lang="scss" scoped>
+.testContainer {
+  width: 40%;
+  margin: 5rem auto;
+  h3 {
+    width: 90%;
+    margin: 0 auto;
+  }
+  form {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    input,
+    textarea {
+      width: 100%;
+      background-color: #3c3e42;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #e6e8eb;
+      margin: 12px 0;
+      color: #fff;
+      padding: 8px 8px;
+    }
+    input:focus,
+    textarea:focus {
+      color: #00adb5;
+      border-bottom: 1px solid #00adb5;
+    }
+    button {
+      background-color: #00adb5;
+      outline: none;
+      border: none;
+      display: flex;
+      align-items: center;
+      padding: 8px 12px;
+      color: #fff;
+      font-weight: 600;
+      width: fit-content;
+      border-radius: 4px;
+      cursor: pointer;
+      img {
+        width: 16px;
+        height: 16px;
+        margin-left: 4px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/AddWork.vue
@@ -0,0 +1,81 @@
+<template>
+  <NavbarComponentVue />
+  <div class="workContainer">
+    <h3>Add Work</h3>
+    <form action="">
+      <input type="text" name="name" id="" placeholder="Name*" required />
+      <input type="text" name="" id="" placeholder="Title*" required />
+      <select name="" id="" required>
+        <option value="">Please Select Category</option>
+        <option value=""></option>
+      </select>
+      <input type="file" />
+      <textarea name="testimonial" placeholder="Testimonial"></textarea>
+      <button type="submit">
+        Submit <img src="../assets/submit_icon.svg" alt="" />
+      </button>
+    </form>
+  </div>
+  <FooterComponentVue />
+</template>
+
+<script setup>
+import NavbarComponentVue from "../components/NavbarComponent.vue";
+import FooterComponentVue from "../components/FooterComponent.vue";
+</script>
+
+<style lang="scss" scoped>
+.workContainer {
+  width: 40%;
+  margin: 3.5rem auto;
+  h3 {
+    width: 90%;
+    margin: 0 auto;
+  }
+  form {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    input,
+    textarea,
+    select {
+      width: 100%;
+      background-color: #3c3e42;
+      outline: none;
+      border: none;
+      border-bottom: 1px solid #e6e8eb;
+      margin: 12px 0;
+      color: #fff;
+      padding: 8px 8px;
+    }
+    select {
+      width: 103%;
+    }
+    input:focus,
+    textarea:focus,
+    select:focus {
+      color: #00adb5;
+      border-bottom: 1px solid #00adb5;
+    }
+    button {
+      background-color: #00adb5;
+      outline: none;
+      border: none;
+      display: flex;
+      align-items: center;
+      padding: 8px 12px;
+      color: #fff;
+      font-weight: 600;
+      width: fit-content;
+      border-radius: 4px;
+      cursor: pointer;
+      img {
+        width: 16px;
+        height: 16px;
+        margin-left: 4px;
+      }
+    }
+  }
+}
+</style>
